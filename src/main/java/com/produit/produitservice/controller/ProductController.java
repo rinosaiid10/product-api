89ac220c846cff1210f88412926f0e582dbf4464
rinosaiid10 <severinkomenan01@gmail.com>
2025-03-16 01:39:37 +0000

    Ajout des Endpoint au CRUD

--- src/main/java/com/produit/produitservice/controller/ProductController.java
+++ src/main/java/com/produit/produitservice/controller/ProductController.java
@@ -10,18 +10,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
-@RequiredArgsConstructor
+
 
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/hello")
-
-    //methode  sayHello
-    public String sayHello() {
-        return  "Hello World";
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
+    /**
+     *
+     * @return endpoint pour recuperer tous les produit
+     */
     @GetMapping("/all")
 
     public List<Product> getAllProducts(){
@@ -29,10 +30,55 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    /**
+     *
+     * @param product
+     * @return endpoint pour creer un produit
+     */
+
     @PostMapping
     public Product createProduct(@RequestBody Product product){
 
         return productService.createProduct(product);
     }
+
+    /**
+     *
+     * @param id
+     * @return endpoint pour recuperer un product
+     */
+
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable long id){
+        return productService.getProductById(id);
+
+    }
+
+    /**
+     *
+     * @param idProduct
+     * @return endpoint pour supprimer un produit
+     */
+
+    @DeleteMapping("{id}")
+    public String deleteProduct(@PathVariable ("id") long idProduct){
+       return  productService.deleteProductById(idProduct);
+    }
+
+    /**
+     *
+     * @param id
+     * @param product
+     * @return endpoint pour modifier un produit
+     */
+
+    @PutMapping("{id}")
+    public Product editProduct(@PathVariable long id, @RequestBody Product product){
+
+        return productService.editProduct(id,product);
+    }
+
+
+
 
 }
--- src/main/java/com/produit/produitservice/service/ProductService.java
+++ src/main/java/com/produit/produitservice/service/ProductService.java
@@ -3,17 +3,23 @@ package com.produit.produitservice.service;
 
 import com.produit.produitservice.model.Product;
 import com.produit.produitservice.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.produit.produitservice.model.Product;
+
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 
 
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
@@ -21,5 +27,38 @@ public class ProductService {
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product getProductById(long id) {
+        Optional<Product> OptionalProduct = productRepository.findById(id);
+
+        if (OptionalProduct.isEmpty()) {
+          throw  new RuntimeException(" Déloser produit inexistente");
+        }
+        return OptionalProduct.get();
+    }
+
+
+    public String deleteProductById(long idProduct) {
+        Optional<Product> OptionalProduct = productRepository.findById(idProduct);
+        if (OptionalProduct.isEmpty()) {
+            throw new RuntimeException("Desoler le produit inexistente, Suppression impossible");
+
+        }
+        productRepository.delete(OptionalProduct.get());
+       return "Prduit supprimé avec succès !";
+    }
+
+    public Product editProduct(long id, Product product) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isEmpty()) {
+            throw new RuntimeException("Produit n'inexistente, la modification impossible");
+        }
+       //stocker le produitAModifier
+        Product productModify = optionalProduct.get();
+
+        productModify.setName(product.getName());
+        productModify.setPrice(product.getPrice());
+        return productRepository.save(productModify);
     }
 }
